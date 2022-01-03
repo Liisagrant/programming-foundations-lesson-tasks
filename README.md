eb367b050d1f9b4d4556312abfbe7b751b709e89
Liisagrant <lisagrant1990@gmail.com>
2022-01-03 15:32:58 +0100

    almoste done with lesson tasks 2, need to look more on last one

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # programming-foundations-lesson-tasks
 
-lesson task 1:
+lesson task 1.1:
 
 -Console log the current day of the week
 
 -Console log 3 multiplied by 6
 
 -Using the alert() method, alert your name, e.g. alert(“Marie”);
+
+Lesson task 1.2 :
+
+-Create a variable called animal and assign it the string value “frog”.
+
+-Create a variable called delivered and assign it the boolean value true.
+
+-Create a variable called score and assign it the number value 12.
+
+-Create a variable called fullName and assign it the result of joining the string values “John” and “Smith.
+
+-Use the typeof operator to find out the type of the fullName variable. Assing the result to a variable called nameType.
--- javascrips.js
+++ javascrips.js
@@ -4,4 +4,16 @@ console.log("Monday");
 //Console log 3 multiplied by 6
 console.log(3*6);
 // Using the alert() method, alert your name, e.g. alert(“Marie”);
-alert("Lisa heiheihei")
+alert("Lisa heiheihei");
+// Lesson task 1.2
+//Create a variable called animal and assign it the string value “frog”.
+var animal = "frog";
+//Create a variable called delivered and assign it the boolean value true.
+var delivered = true;
+//Create a variable called score and assign it the number value 12.
+var score = "12";
+//Create a variable called fullName and assign it the result of joining the string values “John” and “Smith”.
+var firstName = "John";
+var lastName = "Smith";
+var fullName = firstName + lastName; 
+console.log (fullName);
